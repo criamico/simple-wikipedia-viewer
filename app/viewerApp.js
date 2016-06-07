768c38955c790929a4a9063764012fd3416224d0
criamico <criamico@users.noreply.github.com>
2016-06-07 22:57:05 +0100

    urls change when another language is picked

--- app/viewerApp.js
+++ app/viewerApp.js
@@ -4,22 +4,24 @@
         $scope.userQuery = '';
         $scope.language = 'en';
 
+        $scope.changeLang = function(){
+            return $scope.randomArticle = 'https://'+ $scope.language +'.wikipedia.org/wiki/Special:Random';
+        }
 
         $scope.getWikiArticles = function(){
             var wikidata = {};
             var wikiq = '';
             $scope.articlesList = [];
             wikiq = escape($scope.userQuery);
-            /*console.log(wikiq);*/
 
-            /*add some flags to search in different languages (ita, en, spanish, french)*/
+
             var wikiUrl = 'http://'+ $scope.language +'.wikipedia.org/w/api.php?action=query&list=search&srsearch=' + wikiq + '&format=json&callback=JSON_CALLBACK';
 
             $http({url: wikiUrl, callback: "JSON_CALLBACK", method: "JSONP"})
                 .then(function(LoadedData, status, error){
                         LoadedData.data.query.search.forEach(function(element, i){
                             wikidata.title = element.title; //LoadedData.data.query.search[i].title;
-                            wikidata.url = "https://en.wikipedia.org/wiki/" + wikidata.title.replace(/\s/g, "_");
+                            wikidata.url = "https://"+ $scope.language + ".wikipedia.org/wiki/" + wikidata.title.replace(/\s/g, "_");
                             wikidata.content = element.snippet + '...'; //LoadedData.data.query.search[i].snippet;
 
                             $scope.articlesList.push(wikidata);
@@ -34,9 +36,6 @@
             };
 
 
-            $scope.emptyList = function(){
-
-            };
 
 
     });
