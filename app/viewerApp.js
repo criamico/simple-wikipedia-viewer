e946eddc0c9ecec9dae35f9eef31d533c2742d0f
criamico <criamico@users.noreply.github.com>
2016-06-08 13:30:55 +0100

    added autocompletion in input field; so far has a glitch when the user submits the query

--- app/viewerApp.js
+++ app/viewerApp.js
@@ -1,12 +1,32 @@
 (function(){
     angular.module('wikipediaViewer', ['ngSanitize']) /*, 'ngAnimate'*/
-    .controller('viewerController', function($scope, $http){
+    .controller('viewerController', function($scope, $http, $timeout){
         $scope.userQuery = '';
         $scope.language = 'en';
+        $scope.autocompleteList = [];
 
         $scope.changeLang = function(){
             return $scope.randomArticle = 'https://'+ $scope.language +'.wikipedia.org/wiki/Special:Random';
         }
+
+        $scope.autoComplete = function(){
+            if ($scope.userQuery === '')
+                $scope.autocompleteList = [];
+
+            var openSearchURL = "http://" + $scope.language +".wikipedia.org/w/api.php?action=opensearch&search=" + $scope.userQuery + '&format=json&callback=JSON_CALLBACK';
+             $http({url: openSearchURL, callback: "JSON_CALLBACK", method: "JSONP"})
+                .then(function(LoadedData, status, error){
+                    LoadedData.data[1].forEach(function(element, i){
+                        $scope.autocompleteList.push(element);
+                    });
+
+                }, function(LoadedData, status, error){
+                        alert("Sorry the wikipedia opensearch was not successful -status: ", status);
+
+                });
+
+        }
+
 
         $scope.getWikiArticles = function(){
             var wikidata = {};
